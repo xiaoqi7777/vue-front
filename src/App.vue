7a9fdd0ed59ca8f6f2e102d6d53401e4469a4466
xiaoqi7777 <501798525@qq.com>
2019-09-04 07:03:03 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul id="app">
+    <li v-for="(user,index) in users" :key="index">
+      {{user.name}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data(){
+    return{
+      users:[]
+    }
+  },
+  async  mounted(){
+    let users = await axios.get('http://localhost:3000/api/users')
+    this.users = users.data
+  }
+}
+</script>
+
+<style>
+
+</style>
